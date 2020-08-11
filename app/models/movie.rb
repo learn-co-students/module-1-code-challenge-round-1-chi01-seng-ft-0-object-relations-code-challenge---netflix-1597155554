524eca79154e9b9514cac53a5b02a7ce4ce8cf47
mizzygonzo <xeligon@gmail.com>
2020-08-11 11:01:54 -0500

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,21 @@ class Movie
     @@all
   end
 
+  def reviews 
+    Review.all.select do |review_instance|
+      review_instance.movie == self 
+    end 
+  end 
+
+  def reviewers 
+    Viewer.all.select do |viewer_instance|
+      viewer_instance.movie == self 
+    end 
+  end 
+
 end
+
+
+=begin
+
+=end 
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,31 @@ class Viewer
   def self.all
     @@all
   end
+
+  def reviews 
+    Reviews.select do |review_instance|
+      review_instance.viewer == self 
+    end 
+  end  
+  
+  def reviewed_movies 
+    Reviewed_movies.select do |movie_instance|
+      movie_instance.viewer == self 
+    end 
+  end  
+
+  def review_movie 
+    if 
+    else 
+    end 
+  end 
   
 end
+
+
+=begin
+- `Viewer#reviewed_movie?(movie)`
+  - a `Movie` instance is the only argument
+  - returns `true` if the `Viewer` has reviewed this `Movie` (if there is a `Review`
+   instance that has this `Viewer` and `Movie`), returns `false` otherwise
+=end 
