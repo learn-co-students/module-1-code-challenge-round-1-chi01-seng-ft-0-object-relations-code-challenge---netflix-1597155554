ce5c334197aa8ed200f5453efa30c7d9d24a2ce3
Natalia <n8galan@gmail.com>
2020-08-11 09:45:14 -0500

    Initializers, Readers, and Writers

--- tools/console.rb
+++ tools/console.rb
@@ -6,13 +6,28 @@ def reload
   load 'config/environment.rb'
 end
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+# SEED DATA
 
+# MOVIE INSTANCES title
+m1 = Movie.new('Sunless')
+m2 = Movie.new('An American Pickle')
+m3 = Movie.new('Eternal Sunshine of a Spotless Mind')
+m4 = Movie.new('Happy')
 
+# VIEWER INSTANCES username
+v1 = Viewer.new('nataliag')
+v2 = Viewer.new('black_bubbles')
+v3 = Viewer.new('bruce38')
 
+# REVIEW INSTANCES viewer, movie, rating
+r1 = Review.new(v1, m1, 8)
+r2 = Review.new(v2, m1, 8)
+r3 = Review.new(v2, m2, 8)
+r4 = Review.new(v3, m4, 1)
 
+# NOTES
+# Viewer -< Review >- Movie
+# Review is the joiner!
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
 binding.pry
