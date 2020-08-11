11c63d4fc53ce056bd9a7c006f9214cb261adbce
myildiz17 <muhammetyildiz@gmail.com>
2020-08-11 11:01:02 -0500

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,34 @@ class Movie
     @@all
   end
 
+  def reviews
+    Review.all.select do |review_instance|
+      review_instance.movie == self
+    end
+  end
+
+  def reviewers
+    self.reviews.map do |review_instance|
+      review_instance.viewer 
+    end
+  end
+
+  def average_rating
+    total_average = reviews.sum do |review_instance|
+      review_instance.rating
+    end
+    total_average/reviews.count
+  end
+
+  def self.highest_rated
+    Movie.all.max_by do |movie_instance|
+      movie_instance.average_rating
+    end
+  end
 end
+
+
+# Movie#reviews
+# returns an array of all the Review instances for the Movie.
+# Movie#reviewers
+# returns an array of all of the Viewer instances that reviewed the Movie.
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,34 @@ class Viewer
   def self.all
     @@all
   end
+
+  def reviews
+    Review.all.select do |review_instance|
+      review_instance.viewer == self
+    end
+  end
+
+  def reviewed_movies
+    self.reviews.map do |review_instance|
+      review_instance.movie 
+    end
+  end
   
+  def reviewed_movie?(movie)
+    
+    movie_titles= reviewed_movies.select do |movie_instance|
+      movie_instance.title == movie
+    end
+    movie_titles.include?(movie)
+    # binding.pry
+  end
+
+  def rate_movie(movie, rating)
+     if self.reviewed_movies.include?(movie)
+      # binding.pry
+     Review.new(self, movie, rating)
+     end
+  end
 end
+
+
