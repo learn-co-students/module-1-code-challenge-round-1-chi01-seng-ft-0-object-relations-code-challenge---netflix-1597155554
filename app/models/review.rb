3654a5258de227acbd817b3222ff6d0f2dacef2c
KRStoddard <kimberlynrstoddard@gmail.com>
2020-08-11 10:20:46 -0500

    Done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,22 @@
 class Review
 
+    #initialize with viewer instance, movie instance, and rating
+    #track all review instances
+    def initialize(viewer, movie, rating)
+        @viewer = viewer 
+        @movie = movie 
+        @rating = rating 
+
+        @@all << self 
+    end 
+
+    attr_reader :viewer, :movie
+    attr_accessor :rating 
+    @@all = []
+
+    def self.all 
+        @@all 
+    end
+
 
 end
